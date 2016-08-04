d4e439226bf8c807a3ada9beccdc039bd327fbd7
Sbats <simon.bats12@gmail.com>
2016-08-04 16:18:42 +0200

    Huge wwork on scoping modules and fixing few sidecases bugs

--- js/map.js
+++ js/map.js
@@ -1,106 +1,67 @@
-var Lmap = undefined;
-var Mbmap = undefined;
-var timelineEvent = null;
-var colorScale = chroma.scale('OrRd').domain([0, 0.3]);
-var depths = ['0', '109', '266', '541', '1062', '1684', '3220'];
-var depth = depths[0];
-var depthLayers = [];
-var depthLayer = null;
-var baseStyle = {
-    version: 8,
-    attributionControl: true,
-    sources: {
+(function (window) {
+  var L = window.L;
+  var mapboxgl = window.mapboxgl;
+  var $ = window.$;
+
+  window.MapService = function MapService() {
+    var service = this;
+
+    service.Lmap = undefined;
+    service.Mbmap = undefined;
+    service.timelineEvent = null;
+    service.baseStyle = {
+      version: 8,
+      attributionControl: true,
+      sources: {
         tiles_mb: {
-            type: 'raster',
-            tiles: ['data/tiles/tiles_mb/{z}/{x}/{y}.png'],
-            scheme: 'tms',
-            tileSize: 256
+          type: 'raster',
+          tiles: ['data/tiles/tiles_mb/{z}/{x}/{y}.png'],
+          scheme: 'tms',
+          tileSize: 256
         }
-    },
-    layers: [
+      },
+      layers: [
         {
-            id: 'tiles_mb',
-            type: 'raster',
-            source: 'tiles_mb',
-            minzoom: 1,
-            maxzoom: 5
+          id: 'tiles_mb',
+          type: 'raster',
+          source: 'tiles_mb',
+          minzoom: 1,
+          maxzoom: 5
         }
-    ]
-};
+      ]
+    };
 
-function createLeafletMap(options) {
-    Lmap = L.map('map', options);
-}
+    service.createLeafletMap = function createLeafletMap(options) {
+      service.Lmap = L.map('map', options);
+    };
 
-function createMapboxGlMap() {
-    var bounds = [[-100, 0], [20, 80]];
+    service.createMapboxGlMap = function createMapboxGlMap() {
+      var bounds = [[-100, 0], [20, 80]];
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoibWJyb3V0aW4iLCJhIjoiY2lxMmN3MDdjMDA0d2hybTIxOTYxa2c3MCJ9.T5MEIB6UqZLg3_DL4YqDCQ';
+      mapboxgl.accessToken = 'pk.eyJ1IjoibWJyb3V0aW4iLCJhIjoiY2lxMmN3MDdjMDA0d2hybTIxOTYxa2c3MCJ9.T5MEIB6UqZLg3_DL4YqDCQ';
 
 
-    Mbmap = new mapboxgl.Map({
+      service.Mbmap = new mapboxgl.Map({
         container: 'map',
         zoom: 3,
         maxZoom: 4,
         maxBounds: bounds,
-        center: [-70,37],
-        style: baseStyle,
+        center: [-70, 37],
+        style: service.baseStyle,
         attributionControl: {
-            position: 'bottom-right'
+          position: 'bottom-right'
         }
-    });
+      });
+    };
 
-}
+    service.animateTimeline = function animateTimeline(ev) {
+      var target = ev.target;
+      var percent = target.currentTime / target.duration * 100;
+      var progressBar = $('.timeline-progress');
+      progressBar.css({
+        width: percent + '%'
+      });
+    };
+  };
+}(window));
 
-function animateTimeline(ev) {
-    var target = ev.target;
-    var percent = target.currentTime / target.duration * 100;
-    var progressBar = $('.timeline-progress');
-    progressBar.css({
-        'width': percent + '%'
-    });
-}
-
-function depthArrows(feature, latlng) {
-    var html = '<div style="transform: rotate(';
-    html += feature.properties.dir;
-    html += 'deg);';
-    html += 'font-size:';
-    html += (11 + feature.properties.n * 20);
-    html += 'px; color:';
-    html += colorScale(feature.properties.n);
-    html += '" class="rot">';
-    html += '⬆</div>'; //↑⇑
-    return L.marker(latlng, {
-        icon: L.divIcon({
-            className: 'arrow',
-            html: html
-        })
-    });
-};
-
-function initCurrentDepth() {
-    depths.forEach(function (depth) {
-        var currentDepth = L.geoJson(null, {
-            pointToLayer: depthArrows
-        });
-
-        $.getJSON('data/geojson/current_' + depth + '.geojson',
-            function(data) {
-                currentDepth.addData(data);
-            }
-        );
-
-        depthLayers.push(currentDepth);
-    });
-
-    depthLayer = depthLayers[0];
-
-    $('#depthSliderInput').on('slideStop', function(e) {
-        Lmap.removeLayer(depthLayer);
-        depthLayer = depthLayers[e.value];
-        Lmap.addLayer(depthLayer);
-    });
-}
-
-initCurrentDepth();
--- js/mapbox-layers.js
+++ js/mapbox-layers.js
@@ -1,57 +1,63 @@
-var mapboxLayers = {
-    video_current: {
-        type: 'video',
-        urls: [
-            'https://makinacorpus.github.io/gulf-stream-story/data/video/current.mp4'
-        ],
-        coordinates: [
-            [-100, 80],
-            [20, 80],
-            [20, 0],
-            [-100, 0]
-        ]
-    },
-    video_temp: {
-        type: 'video',
-        urls: [
-            'https://makinacorpus.github.io/gulf-stream-story/data/video/temperature.mp4'
-        ],
-        coordinates: [
-            [-100, 80],
-            [20, 80],
-            [20, 0],
-            [-100, 0]
-        ]
-    },
-    video_salinity: {
-        type: 'video',
-        urls: [
-            'https://makinacorpus.github.io/gulf-stream-story/data/video/salinity.mp4'
-        ],
-        coordinates: [
-            [-100, 80],
-            [20, 80],
-            [20, 0],
-            [-100, 0]
-        ]
-    },
-    video_wind: {
-        type: 'video',
-        urls: [
-            'https://makinacorpus.github.io/gulf-stream-story/data/video/wind.mp4'
-        ],
-        coordinates: [
-            [-100, 80],
-            [20, 80],
-            [20, 0],
-            [-100, 0]
-        ]
-    }
-};
+(function (window) {
+  window.MapboxLayers = function MapboxLayers() {
+    this.video_current = {
+      type: 'video',
+      urls: [
+        'https://makinacorpus.github.io/gulf-stream-story/data/video/current.mp4'
+      ],
+      coordinates: [
+          [-100, 80],
+          [20, 80],
+          [20, 0],
+          [-100, 0]
+      ]
+    };
 
-// var mapboxMarkers = {
-//     temperature_sampling: {
-//         coordinates: [-20, 40],
-//         label: 'Point de mesure de la température'
-//     }
-// }
+    this.video_temp = {
+      type: 'video',
+      urls: [
+        'https://makinacorpus.github.io/gulf-stream-story/data/video/temperature.mp4'
+      ],
+      coordinates: [
+          [-100, 80],
+          [20, 80],
+          [20, 0],
+          [-100, 0]
+      ]
+    };
+
+    this.video_salinity = {
+      type: 'video',
+      urls: [
+        'https://makinacorpus.github.io/gulf-stream-story/data/video/salinity.mp4'
+      ],
+      coordinates: [
+          [-100, 80],
+          [20, 80],
+          [20, 0],
+          [-100, 0]
+      ]
+    };
+
+    this.video_wind = {
+      type: 'video',
+      urls: [
+        'https://makinacorpus.github.io/gulf-stream-story/data/video/wind.mp4'
+      ],
+      coordinates: [
+          [-100, 80],
+          [20, 80],
+          [20, 0],
+          [-100, 0]
+      ]
+    };
+
+
+    this.mapboxMarkers = {
+      temperature_sampling: {
+        coordinates: [-20, 40],
+        label: 'Point de mesure de la température'
+      }
+    };
+  };
+}(window));
